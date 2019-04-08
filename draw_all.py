934b7458e7748c2ac970b6b51308a6d9e0645a2a
wqke <47601667+wqke@users.noreply.github.com>
2019-04-08 16:14:20 +0200

    Create draw_all.py

---
+++ draw_all.py
@@ -0,0 +1,13 @@
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib import pyplot as plt
+from numpy import *
+import numpy as np
+import math
+from math import cos,sin,pi
+import root_pandas
+import pandas as pd
+from root_numpy import root2array, rec2array, tree2array
+from ROOT import TFile,TChain,TTree
+from uncertainties import *
+
