368db7d0fbc439193712d148172acc588245d5dd
wqke <47601667+wqke@users.noreply.github.com>
2019-04-10 16:05:55 +0200

    Update background.py

--- background.py
+++ background.py
@@ -26,7 +26,7 @@ frac['prompt']=424/6835.
 names=['B2DstD0X','prompt','B2DstDplusX','B2DstDsX','feed','signal']
 #labels={label1: r'B$\rightarrow D^* D^0 X$', label2: 'prompt',label3: r'B$\rightarrow D^* D^+ X$',label4: r'B$\rightarrow D^* D_s X$',label5: 'feed-down', label6: 'signal'}
 labels=[ r'B$\rightarrow D^* D^0 X$', 'prompt', r'B$\rightarrow D^* D^+ X$', r'B$\rightarrow D^* D_s X$', 'feed-down',  'signal']
-colors=['pink','grey','blue','orange','lightblue','red']
+colors=['#ef10c3','#cecacd','blue','orange','lightblue','red']
 files={'signal':'/home/ke/graphs/signal.root',
       'B2DstD0X':'/home/ke/graphs/B2DstD0X.root',
       'B2DstDplusX':'/home/ke/graphs/B2DstDplusX.root',
@@ -37,15 +37,44 @@ files={'signal':'/home/ke/graphs/signal.root',
 
 
 histo=[]
+histo1=[]
+histo2=[]
+histo3=[]
+
 for i in range(6):
-  df=root_pandas.read_root(files[names[i]],columns=['q2_reco','Tau_life_reco'])
+  df=root_pandas.read_root(files[names[i]],columns=['q2_reco','costheta_L_reco','costheta_D_reco','chi_reco'])
   df=df.sample(n=int(2000000*frac[names[i]]))
   histo.append(df['q2_reco'][~np.isnan(df['q2_reco'])])
-plt.hist(histo,stacked=True,range=[0.,11.],bins=100,label=labels,color=colors)
+  histo1.append(df['costheta_L_reco'][~np.isnan(df['costheta_L_reco'])])
+  histo2.append(df['costheta_D_reco'][~np.isnan(df['costheta_D_reco'])])
+  histo3.append(df['chi_reco'][~np.isnan(df['chi_reco'])])
+plt.hist(histo,stacked=True,range=[0.,11.],bins=8,label=labels,color=colors)
 plt.legend()
 plt.title(r'$q^2$ reco')
 plt.savefig('QTOTAL.pdf')
 plt.close()
+
+plt.hist(histo1,stacked=True,range=[-1.,1.],bins=8,label=labels,color=colors)
+plt.legend()
+plt.title(r'$cos(\theta_L)$ reco')
+plt.savefig('COSTHETAL.pdf')
+plt.close()
+
+
+plt.hist(histo2,stacked=True,range=[-1.,1.],bins=8,label=labels,color=colors)
+plt.legend()
+plt.title(r'$cos(\theta_D)$ reco')
+plt.savefig('COSTHETAD.pdf')
+plt.close()
+
+
+plt.hist(histo3,stacked=True,range=[-np.pi,np.pi],bins=8,label=labels,color=colors)
+plt.legend()
+plt.title(r'$\chi$ reco')
+plt.savefig('CHI.pdf')
+plt.close()
+
+
 
 """
 plt.hist(DF['Tau_life_reco'][~np.isnan(DF['Tau_life_reco'])],histtype='step',range=[0.,2.],bins=100)
