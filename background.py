78971eecae4f34f35c0530a42df5c475b1d7eeef
wqke <47601667+wqke@users.noreply.github.com>
2019-04-10 11:01:06 +0200

    Update background.py

--- background.py
+++ background.py
@@ -23,28 +23,36 @@ frac['prompt']=424/6835.
 
 #Neglect at this stage prompt and feed-down (small fraction)
 #The signal is represented by B2Dsttaunu tau->3pinu purely
-names=['signal','B2DstDsX','B2DstD0X','B2DstDplusX']
-files={'signal':'/data/lhcb/users/hill/bd2dsttaunu_angular/RapidSim_tuples/Bd2DstTauNu/3pi_all_Total/model_vars.root',
+names=['B2DstD0X','prompt','B2DstDplusX','B2DstDsX','feed','signal']
+#labels={label1: r'B$\rightarrow D^* D^0 X$', label2: 'prompt',label3: r'B$\rightarrow D^* D^+ X$',label4: r'B$\rightarrow D^* D_s X$',label5: 'feed-down', label6: 'signal'}
+labels=[ r'B$\rightarrow D^* D^0 X$', 'prompt', r'B$\rightarrow D^* D^+ X$', r'B$\rightarrow D^* D_s X$', 'feed-down',  'signal']
+colors=['pink','grey','blue','orange','lightblue','red']
+files={'signal':'/home/ke/graphs/signal.root',
       'B2DstD0X':'/home/ke/graphs/B2DstD0X.root',
       'B2DstDplusX':'/home/ke/graphs/B2DstDplusX.root',
-      'B2DstDsX':'/home/ke/graphs/B2DstDsX.root'}
+      'B2DstDsX':'/home/ke/graphs/Ds.root',
+      'feed':'/home/ke/graphs/feed.root',
+      'prompt':'/home/ke/graphs/prompt.root'}
 
-DF=root_pandas.read_root(files[names[0]],columns=['q2_reco','Tau_life_reco'])
-DF=DF.sample(n=int(2000000*frac[names[0]]))
-for i in range(1,4):
+
+
+histo=[]
+for i in range(6):
   df=root_pandas.read_root(files[names[i]],columns=['q2_reco','Tau_life_reco'])
   df=df.sample(n=int(2000000*frac[names[i]]))
-  DF=pd.concat([DF, df], ignore_index=True)
-plt.hist(DF['q2_reco'][~np.isnan(DF['q2_reco'])],histtype='step',range=[0.,13.],bins=100)
+  histo.append(df['q2_reco'][~np.isnan(df['q2_reco'])])
+plt.hist(histo,stacked=True,range=[0.,11.],bins=100,label=labels,color=colors)
+plt.legend()
 plt.title(r'$q^2$ reco')
 plt.savefig('QTOTAL.pdf')
 plt.close()
 
+"""
 plt.hist(DF['Tau_life_reco'][~np.isnan(DF['Tau_life_reco'])],histtype='step',range=[0.,2.],bins=100)
 plt.title(r'$\tau$ life time')
 plt.savefig('TAUTOTAL.pdf')
 plt.close()
-
+"""
 
 
 
